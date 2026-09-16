81d36d8979fc2415dde06085334a4a347d4710b9
Pavel K. <pkhatkevich@gmail.com>
2026-09-16 23:20:50 +0300

    remove vercel

---
+++ src/bootstrap.ts
@@ -0,0 +1,9 @@
+import { NestFactory } from "@nestjs/core";
+import { AppModule } from "./app.module";
+
+export const bootstrap = async () => {
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  await app.init();
+  return app;
+};
--- src/main.ts
+++ src/main.ts
@@ -1,34 +1,11 @@
-import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
-import serverlessExpress from '@vendia/serverless-express';
-import { Callback, Context, Handler } from 'aws-lambda';
+import { bootstrap } from './bootstrap';
 
-let cachedServer: Handler;
-
-async function bootstrapServer(): Promise<Handler> {
-  if (!cachedServer) {
-    const nestApp = await NestFactory.create(AppModule);
-    nestApp.enableCors();
-    await nestApp.init();
-    
-    const expressApp = nestApp.getHttpAdapter().getInstance();
-    cachedServer = serverlessExpress({ app: expressApp });
-  }
-  return cachedServer;
-}
-
-export const handler: Handler = async (event: any, context: Context, callback: Callback) => {
-  cachedServer = cachedServer ?? (await bootstrapServer());
-  return cachedServer(event, context, callback);
-};
-
-if (process.env.NODE_ENV !== 'production') {
-  async function bootstrapLocal() {
-    const app = await NestFactory.create(AppModule);
-    app.enableCors();
-    const port = process.env.PORT || 3000;
-    await app.listen(port);
+async function bootstrapLocal() {
+  const app = await bootstrap();
+  const port = process.env.PORT || 3000;
+  await app.listen(port, () => {
     console.log(`🚀 Application is running on: http://localhost:${port}/`);
-  }
-  bootstrapLocal();
+  });
 }
+
+bootstrapLocal();
