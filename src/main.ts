4ba2e06229df122fe8eba3188802c44db8fd65ea
Pavel K. <pkhatkevich@gmail.com>
2026-09-16 15:12:27 +0300

    vercel migration

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,41 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { configure as serverlessExpress } from '@vendia/serverless-express';
+import { Callback, Context, Handler } from 'aws-lambda';
 
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    cors: true,
-    bodyParser: true,
-    logger: ['error', 'warn', 'log', 'verbose', 'debug'],
-  });
-  await app.listen(process.env.PORT ?? 3000);
+let cachedServer: Handler;
+
+async function bootstrapServer(): Promise<Handler> {
+  if (!cachedServer) {
+    const nestApp = await NestFactory.create(AppModule);
+    
+    nestApp.setGlobalPrefix('api');
+    nestApp.enableCors();
+
+    await nestApp.init();
+    
+    const expressApp = nestApp.getHttpAdapter().getInstance();
+    cachedServer = serverlessExpress({ app: expressApp });
+  }
+  return cachedServer;
 }
-void bootstrap();
+
+export const handler: Handler = async (
+  event: any,
+  context: Context,
+  callback: Callback,
+) => {
+  const server = await bootstrapServer();
+  return server(event, context, callback);
+};
+
+if (process.env.NODE_ENV !== 'production') {
+  async function bootstrapLocal() {
+    const app = await NestFactory.create(AppModule);
+    app.setGlobalPrefix('api');
+    app.enableCors();
+    await app.listen(process.env.PORT || 3000);
+    console.log(`🚀 Application is running on: http://localhost:3000/api`);
+  }
+  bootstrapLocal();
+}
