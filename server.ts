54096e5b195e1e4da2f1d4f9fb128e415efc4964
Pavel K. <pkhatkevich@gmail.com>
2026-09-17 12:53:32 +0300

    remove vercel

--- server.ts
+++
@@ -1,7 +0,0 @@
-import { bootstrap } from './src/bootstrap';
-
-export default async function handler(req: Request, res: Response) {
-    const app = await bootstrap();
-    await app.init();
-    return app.getHttpAdapter().getInstance()(req, res);
-};
